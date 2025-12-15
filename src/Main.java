0de26332692e07faa3256f6cc0b5efaa71623c84
Advert <252684@astanait.edu.kz>
2025-12-16 01:18:13 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,64 @@
 public class Main {
     public static void main(String[] args) {
-        Professor professor1 = new Professor(1001,"Advert", "Software Engineering ", 5 );
+        University university = new University("Astana IT University", "Astana", 2019);
+        university.displayUniversityInfo();
 
-        Course se25 = new Course("SE25", "Object-Oriented Programming", 5, "Software Engineering", professor1);
-        professor1.addCourse(se25);
+        Professor prof1 = new Professor(101, "Kydyrbekova Aigerim", "Phd in Comp Science", 10);
+        Professor prof2 = new Professor(102,"Chigimbayeva Diana","Phd in Mathematics", 7);
 
-        se25.displayCourseInfo();
+        Course se201 = new Course("SE201","Object-Oriented Programming",5,"Software Engineering",null);
+        Course math201 = new Course("MATH201","Calculus 2",5,"Software Engineering",null);
 
-        professor1.displayProfessorInfo();
+        university.addProfessor(prof1);
+        university.addProfessor(prof2);
+
+        university.addCourse(se201);
+        university.addCourse(math201);
+
+        prof1.addCourse(se201);
+        prof2.addCourse(math201);
+
+        university.displayAllCourses();
+        university.displayAllProfessors();
+
+        System.out.println("----UPDATED UNIVERSITY INFO----");
+        university.displayUniversityInfo();
+        System.out.println("Average Professor Experience: " + university.getAverageProfessorExp());
+
+        System.out.println("----COMPARISONS----");
+        System.out.println("Who has more years of experience prof1 or prof2?");
+
+        if (prof1.getProfessorYearsOfExp() > prof2.getProfessorYearsOfExp()) {
+            System.out.println("Prof 1 has more Years of experience.");
+        }
+        else if (prof1.getProfessorYearsOfExp() < prof2.getProfessorYearsOfExp()) {
+            System.out.println("Prof 2 has more Years of experience.");
+        }
+        else {
+            System.out.println("Prof 1 and Prof 2 have same years of experience.");
+        }
+
+        System.out.println("Who has more courses teaching?");
+        if (prof1.getCoursesTaught().size() > prof2.getCoursesTaught().size()) {
+            System.out.println("Prof 1 has more courses teaching.");
+        }
+        else if (prof1.getCoursesTaught().size() < prof2.getCoursesTaught().size()) {
+            System.out.println("Prof 2 has more courses teaching.");
+        }
+        else {
+            System.out.println("Prof 1 and Prof 2 have same amount of courses teaching.");
+        }
+
+        System.out.println("Which course has more credits assigned?");
+        if (se201.getCredits() > math201.getCredits()) {
+            System.out.println(se201.getCourseName() + " Has more credits assigned!");
+        }
+        else if (se201.getCredits() < math201.getCredits()) {
+            System.out.println(math201.getCourseName() + " has more credits assigned!");
+        }
+        else {
+            System.out.println("Both courses have same amount of credits assigned.");
+        }
+
     }
 }
--- src/University.java
+++ src/University.java
@@ -1,3 +1,127 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class University {
+    private String name;
+    private String location;
+    private int foundedYear;
+    private List<Course> courses;
+    private List<Professor> professors;
+
+    public University() {
+        this.name = "";
+        this.location = "";
+        this.foundedYear = 0;
+        this.courses = new ArrayList<>();
+        this.professors = new ArrayList<>();
+    }
+
+    public University(String name, String location, int foundedYear) {
+        this.name = name;
+        this.location = location;
+        this.foundedYear = foundedYear;
+        this.courses = new ArrayList<>();
+        this.professors = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getFoundedYear() {
+        return foundedYear;
+    }
+
+    public void setFoundedYear(int foundedYear) {
+        this.foundedYear = foundedYear;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public List<Professor> getProfessors() {
+        return professors;
+    }
+
+    public void addCourse(Course course) {
+        if (course != null && !courses.contains(course)) {
+            courses.add(course);
+        }
+    }
+
+    public void addProfessor(Professor professor) {
+        if (professor != null && !professors.contains(professor)) {
+            professors.add(professor);
+        }
+    }
+
+    public void removeCourse(Course course) {
+        courses.remove(course);
+    }
+
+    public void removeProfessor(Professor professor) {
+        professors.remove(professor);
+    }
+
+    public double getAverageProfessorExp() {
+        if (professors.isEmpty()) {
+            return 0.0;
+        }
+        else {
+            int totalexp = 0;
+            for (Professor professor : professors) {
+                totalexp += professor.getProfessorYearsOfExp();
+            }
+            return (double) totalexp / professors.size();
+
+        }
+    }
+
+    public void displayUniversityInfo() {
+        System.out.println("=== UNIVERSITY INFORMATION ===");
+        System.out.println("Name: " + name);
+        System.out.println("Location: " + location);
+        System.out.println("Founded: " + foundedYear);
+        System.out.println("Number of Courses: " + courses.size());
+        System.out.println("Number of Professors: " + professors.size());
+        System.out.println("-----------------------------");
+    }
+
+    public void displayAllCourses() {
+        System.out.println("=== ALL COURSES ===");
+        if (courses.isEmpty()) {
+            System.out.println("No courses available.");
+        } else {
+            for (int i = 0; i < courses.size(); i++) {
+                System.out.print((i + 1) + ". ");
+                courses.get(i).displayCourseInfo();
+            }
+        }
+    }
+
+    public void displayAllProfessors() {
+        System.out.println("\n=== ALL PROFESSORS ===");
+        if (professors.isEmpty()) {
+            System.out.println("No professors available.");
+        } else {
+            for (Professor professor : professors) {
+                professor.displayProfessorInfo();
+                System.out.println();
+            }
+        }
+    }
 
 }
