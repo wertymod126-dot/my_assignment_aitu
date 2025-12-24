8b93b13b74e2930c370ab51cac043a4404191414
Advert <252684@astanait.edu.kz>
2025-12-25 00:40:58 +0500

    changes

---
+++ src/Person.java
@@ -0,0 +1,34 @@
+public abstract class Person {
+    protected int id;
+    protected String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public abstract void displayInfo();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String ToString() {
+        return name + "(ID: " + id + " )";
+    }
+
+
+}
